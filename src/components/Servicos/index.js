da6f600074647bc61b53077ff2c258fd6b33f6e2
Juarez <juarez.silva@mlins.com.br>
2021-06-30 16:50:59 -0300

    fix: download receipts on Android

--- src/components/Servicos/index.js
+++ src/components/Servicos/index.js
@@ -36,9 +36,11 @@ function Servicos() {
           <li>
             <h4>Opções</h4>
             <p>Captação de recursos, etc.</p>
+            <a download>
               <button onClick={() => print()}>
-                Clique
+                Clique last
               </button>
+            </a>
           </li>
         </ul>
       </div>
